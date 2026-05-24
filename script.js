a2c7c70ed47869bf5876632c851ab2a79698317e
HushaamShah <hasham393@gmail.com>
2026-05-24 16:18:57 +0500

    socket communication done; node:sqlite db added

--- script.js
+++ script.js
@@ -3,5 +3,22 @@ const inputBox = document.getElementById("inputBox");
 inputBox.addEventListener("input", change);
 
 function change(event){
-    console.log(event)
+    console.log(event.data)
+    let obj = {
+        msg: event.target.value
+    };
+    socket.send(JSON.stringify(obj));
 }
+
+const socket = new WebSocket('ws://localhost:8080/');
+
+socket.onopen = () => {
+    let obj = {
+        msg: 'Hello Server!'
+    };
+    socket.send(JSON.stringify(obj));
+};
+socket.onmessage = (event) => {
+    console.log('Server says:', event.data);
+    inputBox.value = JSON.parse(event.data).msg
+};
--- server.js
+++ server.js
@@ -1,18 +1,52 @@
-let http = require('http');
-let fs = require('fs');
-http.createServer(function (req, res) {
-    console.log('Server Running')
-    if (req.url == '/') {
-        serveFiles('/index.html', res)
+import http from 'http';
+import fs from 'fs';
+import { WebSocketServer } from 'ws';
+import { DatabaseSync } from 'node:sqlite';
+
+
+
+const middlewares = [
+    (req, res, next) => {
+        let body = ''
+        req.on('data', chunk => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            req.body = body ? JSON.parse(body) : {};
+            next();
+        })
     }
-    if (req.url == '/script.js') {
-        serveFiles(req.url, res)
-    }
-    if (req.url == '/test') {
-        console.log('test')
+];
+
+const server = http.createServer(function (req, res) {
+    let index = 0;
+
+    function next() {
+        if (index < middlewares.length) {
+            const middleware = middlewares[index++];
+            middleware(req, res, next);
+        } else {
+            console.log('Server Running')
+            console.log(req.url)
+            if (req.url == '/') {
+                serveFiles('/index.html', res)
+            }
+            if (req.url == '/script.js') {
+                serveFiles(req.url, res)
+            }
+            if (req.url == '/test') {
+                console.log('test')
+            }
+            if (req.url == '/test') {
+                console.log('test')
+            }
+        }
     }
 
-}).listen(8080);
+    next();
+
+}).listen(8080);;
 
 function serveFiles(filePath, res) {
     fs.readFile(`.${filePath}`, (err, data) => {
@@ -25,3 +59,60 @@ function serveFiles(filePath, res) {
         }
     })
 }
+
+
+
+const wss = new WebSocketServer({ server });
+
+wss.on('error', function logError(error) {
+    console.log('ERROR!!!!!!!')
+    console.error(error)
+});
+
+// ws.on('open', function open() {
+//   ws.send('something');
+// });
+
+// wss.on('connection', (ws, req) => {
+//     ws.on('message', (message) => {
+//         console.log(message)
+//         // ws.send(JSON.stringify(JSON.parse(message)))
+//         broadcastToAll({ text: `Someone said: ${message}` });
+//     })
+
+//     console.log('connection')
+// });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    wss.clients.forEach((client) => {
+      // Check that the client is open AND is not the original sender
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(`${message}`);
+      }
+    });
+  });
+});
+
+function broadcastToAll(data) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+}
+
+// ws.on('message', function message(data) {
+//   console.log('received: %s', data);
+// });
+
+
+
+
+
+
+
+const database = new DatabaseSync('./appDB');
+const query = database.prepare('SELECT * FROM data');
+// Execute the prepared statement and log the result set.
+console.log(query.all());
